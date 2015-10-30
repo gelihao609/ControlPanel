a82de2007879fc3d98ace95d48bb01c918386c33
Alex Ge <gelihao@gmail.com>
2015-10-30 01:15:28 -0700

    xmlDescription

--- boundary/LoaderGateway.java
+++ boundary/LoaderGateway.java
@@ -2,8 +2,34 @@ package boundary;
 
 import Entity.ILoader;
 
-/**
+/**Gateway used to load XML into objects and parse object into XML
  * 
+ * 
+ */
+/*
+ * Project XML:
+ * <Project>
+ * 	<ID><\ID>
+ * 	<Name><\Name>
+ * 	<predecessors><\predecessors>
+ * 		<Task><\Task>
+ * 	<successors><\successors>
+ * 		<Task><\Task>
+ * 	<children><\children>
+ * 		<Task><\Task>
+ * 	<parent><\parent>
+ * 		<Task><\Task>
+ * 	<TaskPool>
+ * 		<Task>
+ * 			<ID><\ID>
+ * 			<Name><\Name>
+ * 		<\Task>
+ * 	<\TaskPool>
+ * 	<ResourcePool>
+ * 		<Resource>
+ * 		<\Resource>
+ * 	<\ResourcePool>
+ * <\Project>
  */
 public class LoaderGateway implements ILoader {
 
